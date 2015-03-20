0b7da01da2c9a57bd7259eb1f8ed467c850b2320
Karl Isenberg <kisenberg@pivotal.io>
2015-03-19 17:35:10 -0700

    Implement grant & revoke
    [#88665590]

--- database/database.go
+++ database/database.go
@@ -1,8 +1,10 @@
 package database
 
 import (
-    "github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/sql"
-    "fmt"
+	"fmt"
+
+	"github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/sql"
+	"github.com/pivotal-golang/lager"
 )
 
 const revokeQuery = `UPDATE mysql.db
@@ -14,61 +16,117 @@ SET Insert_priv = 'Y', Update_priv = 'Y', Create_priv = 'Y'
 WHERE Db = ?`
 
 type Database interface {
-    GrantPrivileges() error
-    RevokePrivileges() error
-    ResetActivePrivileges() error
+	GrantPrivileges() error
+	RevokePrivileges() error
+	ResetActivePrivileges() error
 }
 
-type database struct{
-    name string
-    db sql.DB
+type database struct {
+	name   string
+	db     sql.DB
+	logger lager.Logger
 }
 
-func New(name string, db sql.DB) Database {
+func New(name string, db sql.DB, logger lager.Logger) Database {
 	return database{
-        name: name,
-        db: db,
-    }
+		name:   name,
+		db:     db,
+		logger: logger,
+	}
 }
 
 func (d database) RevokePrivileges() error {
-    result, err := d.db.Exec(revokeQuery, d.name)
-    if err != nil {
-        return fmt.Errorf("Updating db '%s' to revoke priviledges: %s", d.name, err.Error())
-    }
+	d.logger.Info(fmt.Sprintf("Revoking priviledges to db '%s'", d.name))
 
-    //TODO: do we even care about rows affected? Is db name the PK or will there be multiple rows, one per user with access?
-    rowsAffected, err := result.RowsAffected()
-    if err != nil {
-        return fmt.Errorf("Updating db '%s' to revoke priviledges: Getting rows affected: %s", d.name, err.Error())
-    }
+	result, err := d.db.Exec(revokeQuery, d.name)
+	if err != nil {
+		return fmt.Errorf("Updating db '%s' to revoke priviledges: %s", d.name, err.Error())
+	}
 
-    if rowsAffected != 1 {
-        return fmt.Errorf("Updating db '%s' to revoke priviledges: Affected %d rows, expected 1", d.name, rowsAffected)
-    }
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Updating db '%s' to revoke priviledges: Getting rows affected: %s", d.name, err.Error())
+	}
 
-    return nil
+	d.logger.Info(fmt.Sprintf("Updating db '%s' to revoke priviledges: Rows affected: %s", d.name, rowsAffected))
+
+	_, err = d.db.Exec("FLUSH PRIVILEGES")
+	if err != nil {
+		return fmt.Errorf("Flushing privileges: %s", err.Error())
+	}
+
+	return nil
 }
 
 func (d database) GrantPrivileges() error {
-    result, err := d.db.Exec(grantQuery, d.name)
-    if err != nil {
-        return fmt.Errorf("Updating db '%s' to grant priviledges: %s", d.name, err.Error())
-    }
+	d.logger.Info(fmt.Sprintf("Granting priviledges to db '%s'", d.name))
 
-    //TODO: do we even care about rows affected? Is db name the PK or will there be multiple rows, one per user with access?
-    rowsAffected, err := result.RowsAffected()
-    if err != nil {
-        return fmt.Errorf("Updating db '%s' to grant priviledges: Getting rows affected: %s", d.name, err.Error())
-    }
+	result, err := d.db.Exec(grantQuery, d.name)
+	if err != nil {
+		return fmt.Errorf("Updating db '%s' to grant priviledges: %s", d.name, err.Error())
+	}
 
-    if rowsAffected != 1 {
-        return fmt.Errorf("Updating db '%s' to grant priviledges: Affected %d rows, expected 1", d.name, rowsAffected)
-    }
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Updating db '%s' to grant priviledges: Getting rows affected: %s", d.name, err.Error())
+	}
 
-    return nil
+	d.logger.Info(fmt.Sprintf("Updating db '%s' to grant priviledges: Rows affected: %s", d.name, rowsAffected))
+
+	_, err = d.db.Exec("FLUSH PRIVILEGES")
+	if err != nil {
+		return fmt.Errorf("Flushing privileges: %s", err.Error())
+	}
+
+	return nil
 }
 
+/*
+#
+      # In order to change privileges immediately, we must do two things:
+      # 1) Flush the privileges
+      # 2) Kill any and all active connections
+      #
+      def reset_active_privileges(database)
+        connection.execute('FLUSH PRIVILEGES')
+
+        processes = connection.select('SHOW PROCESSLIST')
+        processes.each do |process|
+          id, db, user = process.values_at('Id', 'db', 'User')
+
+          if db == database && user != 'root'
+            begin
+              connection.execute("KILL CONNECTION #{id}")
+            rescue ActiveRecord::StatementInvalid => e
+              raise unless e.message =~ /Unknown thread id/
+            end
+          end
+        end
+      end
+*/
+// ResetActivePrivileges flushes the privileges and kills all active connections to this database.
+// New connections will get the new privileges.
 func (d database) ResetActivePrivileges() error {
+	//
+	//    rows, err := d.db.Query("SELECT ID FROM INFORMATION_SCHEMA.PROCESSLIST WHERE DB = ? AND USER <> ?", d.name, "root")
+	//    if err != nil {
+	//        return fmt.Errorf("Getting list of open connections to database '%s': %s", d.name, err.Error())
+	//    }
+	//    defer rows.Close()
+	//    for rows.Next() {
+	//        var connectionID string
+	//        if err := rows.Scan(&connectionID); err != nil {
+	//            return fmt.Errorf("Scanning open connections to database '%s': %s", d.name, err.Error())
+	//        }
+	//
+	//        _, err := d.db.Exec("KILL CONNECTION ?", connectionID) //TODO: result?
+	//        if err != nil {
+	//            return fmt.Errorf("Killing connection: %s", err.Error())
+	//        }
+	//    }
+	//    if err := rows.Err(); err != nil {
+	//        return fmt.Errorf("Reading open connections to database '%s': %s", d.name, err.Error())
+	//    }
+
 	return nil
 }
--- database/database_test.go
+++ database/database_test.go
@@ -3,234 +3,192 @@ package database_test
 import (
 	. "github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/database"
 
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
-    "errors"
+	"errors"
 
-    sqlfakes "github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/sql/fakes"
+	"database/sql"
+
+	sqlfakes "github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/sql/fakes"
+	"github.com/pivotal-golang/lager/lagertest"
 )
 
 var _ = Describe("Database", func() {
 
-    const dbName = "fake-db-name"
-    var database Database
-    var fakeDB *sqlfakes.FakeDB
+	const dbName = "fake-db-name"
+	var (
+		logger   *lagertest.TestLogger
+		database Database
+		fakeDB   *sqlfakes.FakeDB
+	)
 
-    BeforeEach(func() {
-        fakeDB = &sqlfakes.FakeDB{}
-        database = New(dbName, fakeDB)
-    })
+	BeforeEach(func() {
+		logger = lagertest.NewTestLogger("ProxyRunner test")
+		fakeDB = &sqlfakes.FakeDB{}
+		database = New(dbName, fakeDB, logger)
+	})
 
-    Describe("RevokePrivileges", func() {
-        var fakeResult *sqlfakes.FakeResult
+	Describe("RevokePrivileges", func() {
+		var fakeResult *sqlfakes.FakeResult
 
-        BeforeEach(func() {
-            fakeResult = &sqlfakes.FakeResult{}
-            fakeDB.ExecReturns(fakeResult, nil)
+		BeforeEach(func() {
+			fakeResult = &sqlfakes.FakeResult{}
+			fakeDB.ExecReturns(fakeResult, nil)
 
-            fakeResult.RowsAffectedReturns(1, nil)
-        })
+			fakeResult.RowsAffectedReturns(1, nil)
+		})
 
-        It("makes a sql query to revoke priveledges on a database", func() {
-            err := database.RevokePrivileges()
-            Expect(err).ToNot(HaveOccurred())
+		It("makes a sql query to revoke priveledges on a database and then flushes privileges", func() {
+			err := database.RevokePrivileges()
+			Expect(err).ToNot(HaveOccurred())
 
-            Expect(fakeDB.ExecCallCount()).To(Equal(1))
+			Expect(fakeDB.ExecCallCount()).To(Equal(2))
 
-            query, args := fakeDB.ExecArgsForCall(0)
-            Expect(query).To(Equal(`UPDATE mysql.db
+			query, args := fakeDB.ExecArgsForCall(0)
+			Expect(query).To(Equal(`UPDATE mysql.db
 SET Insert_priv = 'N', Update_priv = 'N', Create_priv = 'N'
 WHERE Db = ?`))
-            Expect(args).To(Equal([]interface{}{dbName}))
-        })
+			Expect(args).To(Equal([]interface{}{dbName}))
 
-        Context("when the query fails", func() {
-            BeforeEach(func() {
-                fakeDB.ExecReturns(nil, errors.New("fake-query-error"))
-            })
+			query, args = fakeDB.ExecArgsForCall(1)
+			Expect(query).To(Equal("FLUSH PRIVILEGES"))
+			Expect(args).To(BeEmpty())
+		})
 
-            It("returns an error", func() {
-                err := database.RevokePrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("fake-query-error"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		Context("when the query fails", func() {
+			BeforeEach(func() {
+				fakeDB.ExecReturns(nil, errors.New("fake-query-error"))
+			})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-            })
-        })
+			It("returns an error", func() {
+				err := database.RevokePrivileges()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-query-error"))
+				Expect(err.Error()).To(ContainSubstring(dbName))
 
-        Context("when getting the number of affected rows fails", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(0, errors.New("fake-rows-affected-error"))
-            })
+				Expect(fakeDB.ExecCallCount()).To(Equal(1))
+			})
+		})
 
-            It("returns an error", func() {
-                err := database.RevokePrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("fake-rows-affected-error"))
-                Expect(err.Error()).To(ContainSubstring("Getting rows affected"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		Context("when getting the number of affected rows fails", func() {
+			BeforeEach(func() {
+				fakeResult.RowsAffectedReturns(0, errors.New("fake-rows-affected-error"))
+			})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
+			It("returns an error", func() {
+				err := database.RevokePrivileges()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-rows-affected-error"))
+				Expect(err.Error()).To(ContainSubstring("Getting rows affected"))
+				Expect(err.Error()).To(ContainSubstring(dbName))
 
-        Context("when the update affects zero rows", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(0, nil)
-            })
+				Expect(fakeDB.ExecCallCount()).To(Equal(1))
+				Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
+			})
+		})
 
-            It("returns an error", func() {
-                err := database.RevokePrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("Affected 0 rows, expected 1"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		Context("when flushing privileges fails", func() {
+			BeforeEach(func() {
+				fakeDB.ExecStub = func(query string, args ...interface{}) (sql.Result, error) {
+					if query == "FLUSH PRIVILEGES" {
+						return nil, errors.New("fake-flush-error")
+					}
+					return fakeResult, nil
+				}
+			})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
+			It("returns an error", func() {
+				err := database.RevokePrivileges()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-flush-error"))
 
-        Context("when the update affects more than one row", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(2, nil)
-            })
+				Expect(fakeDB.ExecCallCount()).To(Equal(2))
+			})
+		})
+	})
 
-            It("returns an error", func() {
-                err := database.RevokePrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("Affected 2 rows, expected 1"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+	Describe("GrantPrivileges", func() {
+		var fakeResult *sqlfakes.FakeResult
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
+		BeforeEach(func() {
+			fakeResult = &sqlfakes.FakeResult{}
+			fakeDB.ExecReturns(fakeResult, nil)
 
-        Context("when the update affects a negative number of rows", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(-1, nil)
-            })
+			fakeResult.RowsAffectedReturns(1, nil)
+		})
 
-            It("returns an error", func() {
-                err := database.RevokePrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("Affected -1 rows, expected 1"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		It("Expects priviledges to be granted to the database", func() {
+			err := database.GrantPrivileges()
+			Expect(err).ToNot(HaveOccurred())
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
-    })
+			Expect(fakeDB.ExecCallCount()).To(Equal(2))
 
-    Describe("GrantPrivileges", func() {
-        var fakeResult *sqlfakes.FakeResult
-
-        BeforeEach(func() {
-            fakeResult = &sqlfakes.FakeResult{}
-            fakeDB.ExecReturns(fakeResult, nil)
-
-            fakeResult.RowsAffectedReturns(1, nil)
-        })
-
-        It("Expects priviledges to be granted to the database", func() {
-            err := database.GrantPrivileges()
-            Expect(err).ToNot(HaveOccurred())
-
-            Expect(fakeDB.ExecCallCount()).To(Equal(1))
-
-            query, args := fakeDB.ExecArgsForCall(0)
-            Expect(query).To(Equal(`UPDATE mysql.db
+			query, args := fakeDB.ExecArgsForCall(0)
+			Expect(query).To(Equal(`UPDATE mysql.db
 SET Insert_priv = 'Y', Update_priv = 'Y', Create_priv = 'Y'
 WHERE Db = ?`))
-            Expect(args).To(Equal([]interface{}{dbName}))
-        })
+			Expect(args).To(Equal([]interface{}{dbName}))
 
-        Context("when the query fails", func() {
-            BeforeEach(func() {
-                fakeDB.ExecReturns(nil, errors.New("fake-query-error"))
-            })
+			query, args = fakeDB.ExecArgsForCall(1)
+			Expect(query).To(Equal("FLUSH PRIVILEGES"))
+			Expect(args).To(BeEmpty())
+		})
 
-            It("returns an error", func() {
-                err := database.GrantPrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("fake-query-error"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		Context("when the query fails", func() {
+			BeforeEach(func() {
+				fakeDB.ExecReturns(nil, errors.New("fake-query-error"))
+			})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-            })
-        })
+			It("returns an error", func() {
+				err := database.GrantPrivileges()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-query-error"))
+				Expect(err.Error()).To(ContainSubstring(dbName))
 
-        Context("when getting the number of affected rows fails", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(0, errors.New("fake-rows-affected-error"))
-            })
+				Expect(fakeDB.ExecCallCount()).To(Equal(1))
+			})
+		})
 
-            It("returns an error", func() {
-                err := database.GrantPrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("fake-rows-affected-error"))
-                Expect(err.Error()).To(ContainSubstring("Getting rows affected"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		Context("when getting the number of affected rows fails", func() {
+			BeforeEach(func() {
+				fakeResult.RowsAffectedReturns(0, errors.New("fake-rows-affected-error"))
+			})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
+			It("returns an error", func() {
+				err := database.GrantPrivileges()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-rows-affected-error"))
+				Expect(err.Error()).To(ContainSubstring("Getting rows affected"))
+				Expect(err.Error()).To(ContainSubstring(dbName))
 
-        Context("when the update affects zero rows", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(0, nil)
-            })
+				Expect(fakeDB.ExecCallCount()).To(Equal(1))
+				Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
+			})
+		})
 
-            It("returns an error", func() {
-                err := database.GrantPrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("Affected 0 rows, expected 1"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+		Context("when flushing privileges fails", func() {
+			BeforeEach(func() {
+				fakeDB.ExecStub = func(query string, args ...interface{}) (sql.Result, error) {
+					if query == "FLUSH PRIVILEGES" {
+						return nil, errors.New("fake-flush-error")
+					}
+					return fakeResult, nil
+				}
+			})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
+			It("returns an error", func() {
+				err := database.GrantPrivileges()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-flush-error"))
 
-        Context("when the update affects more than one row", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(2, nil)
-            })
+				Expect(fakeDB.ExecCallCount()).To(Equal(2))
+			})
+		})
 
-            It("returns an error", func() {
-                err := database.GrantPrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("Affected 2 rows, expected 1"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
+	})
 
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
+	Describe("ResetActivePrivileges", func() {
 
-        Context("when the update affects a negative number of rows", func() {
-            BeforeEach(func() {
-                fakeResult.RowsAffectedReturns(-1, nil)
-            })
-
-            It("returns an error", func() {
-                err := database.GrantPrivileges()
-                Expect(err).To(HaveOccurred())
-                Expect(err.Error()).To(ContainSubstring("Affected -1 rows, expected 1"))
-                Expect(err.Error()).To(ContainSubstring(dbName))
-
-                Expect(fakeDB.ExecCallCount()).To(Equal(1))
-                Expect(fakeResult.RowsAffectedCallCount()).To(Equal(1))
-            })
-        })
-    })
-
-    XDescribe("ResetActivePrivileges", func() {
-        //TODO: write tests
-    })
+	})
 })
