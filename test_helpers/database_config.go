32daf238e1f3ceb63b590198648a81a6cd56048b
Lyle Franklin and Rob Dimsdale <rdimsdale@pivotal.io>
2015-04-07 10:14:16 -0700

    Load same config in test as in main.
    [#88665590]

--- test_helpers/database_config.go
+++ test_helpers/database_config.go
@@ -1,13 +1,28 @@
 package test_helpers
 
-import "github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/database"
+import (
+	"os"
+
+	"github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/config"
+	"github.com/pivotal-cf-experimental/cf-mysql-quota-enforcer/database"
+)
 
 func NewRootDatabaseConfig(dbName string) database.Config {
+	configPath := os.Getenv("CONFIG")
+	if configPath == "" {
+		panic("CONFIG path must be specified")
+	}
+
+	config, err := config.Load(configPath)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	return database.Config{
-		Host:     "127.0.0.1",
-		Port:     3306,
-		User:     "root",
-		Password: "password",
+		Host:     config.Host,
+		Port:     config.Port,
+		User:     config.User,
+		Password: config.Password,
 		DBName:   dbName,
 	}
 }
