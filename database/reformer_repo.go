01e4a48127b8653719e356a6cacee3b06f42b6d3
Andrew Crump <acrump@pivotal.io>
2016-10-19 17:15:37 -0700

    Remove special-case handling for empty ignored users
    - Ignored users are always populated with the username of the db user
    that the quota enforcer runs as

--- database/reformer_repo.go
+++ database/reformer_repo.go
@@ -27,31 +27,9 @@ FROM (
 	HAVING ROUND(SUM(COALESCE(tables.data_length + tables.index_length,0) / 1024 / 1024), 1) < MAX(instances.max_storage_mb)
 ) AS reformers
 `
-const reformersQueryPatternNoIgnoredUsers = `
-SELECT reformers.name AS reformer_db, reformers.user AS reformer_user
-FROM (
-	SELECT violator_dbs.name, violator_dbs.user, tables.data_length, tables.index_length
-	FROM   (
-		SELECT DISTINCT table_schema as name, replace(substring_index(grantee, '@', 1), "'", '') AS user
-		FROM information_schema.schema_privileges
-		WHERE privilege_type IN ('SELECT', 'INSERT', 'UPDATE', 'CREATE')
-		GROUP BY grantee, table_schema
-		HAVING count(*) != 4
-	) AS violator_dbs
-	JOIN        %s.service_instances AS instances ON violator_dbs.name = instances.db_name COLLATE utf8_general_ci
-	LEFT JOIN   information_schema.tables AS tables ON tables.table_schema = violator_dbs.name
-	GROUP  BY   violator_dbs.user
-	HAVING ROUND(SUM(COALESCE(tables.data_length + tables.index_length,0) / 1024 / 1024), 1) < MAX(instances.max_storage_mb)
-) AS reformers
-`
 
 func NewReformerRepo(brokerDBName string, ignoredUsers []string, db *sql.DB, logger lager.Logger) Repo {
-	if len(ignoredUsers) > 0 {
-		ignoredUsersPlaceholders := strings.Join(strings.Split(strings.Repeat("?", len(ignoredUsers)), ""), ",")
-		query := fmt.Sprintf(reformersQueryPattern, ignoredUsersPlaceholders, brokerDBName)
-		return newRepo(query, ignoredUsers, db, logger, "quota reformer")
-	} else {
-		query := fmt.Sprintf(reformersQueryPatternNoIgnoredUsers, brokerDBName)
-		return newRepo(query, nil, db, logger, "quota reformer")
-	}
+	ignoredUsersPlaceholders := strings.Join(strings.Split(strings.Repeat("?", len(ignoredUsers)), ""), ",")
+	query := fmt.Sprintf(reformersQueryPattern, ignoredUsersPlaceholders, brokerDBName)
+	return newRepo(query, ignoredUsers, db, logger, "quota reformer")
 }
--- database/violator_repo.go
+++ database/violator_repo.go
@@ -25,29 +25,8 @@ FROM (
 ) AS violators
 `
 
-const violatorsQueryPatternNoIgnoredUsers = `
-SELECT violators.name AS violator_db, violators.user AS violator_user
-FROM (
-	SELECT dbs.name, dbs.user, tables.data_length, tables.index_length
-	FROM   (
-		SELECT DISTINCT table_schema AS name, replace(substring_index(grantee, '@', 1), "'", '') AS user
-		FROM information_schema.schema_privileges
-		WHERE privilege_type IN ('INSERT', 'UPDATE', 'CREATE')
-	) AS dbs
-	JOIN %s.service_instances AS instances ON dbs.name = instances.db_name COLLATE utf8_general_ci
-	JOIN information_schema.tables AS tables ON tables.table_schema = dbs.name
-	GROUP BY dbs.user
-	HAVING ROUND(SUM(COALESCE(tables.data_length + tables.index_length,0) / 1024 / 1024), 1) >= MAX(instances.max_storage_mb)
-) AS violators
-`
-
 func NewViolatorRepo(brokerDBName string, ignoredUsers []string, db *sql.DB, logger lager.Logger) Repo {
-	if len(ignoredUsers) > 0 {
-		ignoredUsersPlaceholders := strings.Join(strings.Split(strings.Repeat("?", len(ignoredUsers)), ""), ",")
-		query := fmt.Sprintf(violatorsQueryPattern, ignoredUsersPlaceholders, brokerDBName)
-		return newRepo(query, ignoredUsers, db, logger, "quota violator")
-	} else {
-		query := fmt.Sprintf(violatorsQueryPatternNoIgnoredUsers, brokerDBName)
-		return newRepo(query, nil, db, logger, "quota reformer")
-	}
+	ignoredUsersPlaceholders := strings.Join(strings.Split(strings.Repeat("?", len(ignoredUsers)), ""), ",")
+	query := fmt.Sprintf(violatorsQueryPattern, ignoredUsersPlaceholders, brokerDBName)
+	return newRepo(query, ignoredUsers, db, logger, "quota violator")
 }
